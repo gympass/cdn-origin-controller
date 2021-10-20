b997be489ca0b1a2a9633db66eb44e59197fb840
Caio Almeida <caio.f.r.amd@gmail.com>
2021-10-20 14:32:38 -0300

    Add configurations to setup how controller will create a distribution
    fix price class reference

--- internal/config/config.go
+++ internal/config/config.go
@@ -20,17 +20,42 @@
 package config
 
 import (
+	"strings"
+
+	awscloudfront "github.com/aws/aws-sdk-go/service/cloudfront"
 	"github.com/spf13/viper"
 )
 
 const (
-	logLevelKey = "log_level"
-	devModeKey  = "dev_mode"
+	logLevelKey              = "log_level"
+	devModeKey               = "dev_mode"
+	cfDefaultOriginDomain    = "cf_default_origin_domain"
+	cfPriceClassKey          = "cf_price_class"
+	cfWafArnKey              = "cf_aws_waf"
+	cfCustomSSLCertArnKey    = "cf_custom_ssl_cert"
+	cfSecurityPolicyKey      = "cf_security_policy"
+	cfEnableLoggingKey       = "cf_enable_logging"
+	cfS3BucketLogKey         = "cf_s3_bucket_log"
+	cfEnableIPV6Key          = "cf_enable_ipv6"
+	cfDescriptionTemplateKey = "cf_description_template"
+	cfAliasCreationKey       = "cf_alias_creation"
+	cfCustomTagsKey          = "cf_custom_tags"
 )
 
 func init() {
 	viper.SetDefault(logLevelKey, "info")
 	viper.SetDefault(devModeKey, "false")
+	viper.SetDefault(cfDefaultOriginDomain, "")
+	viper.SetDefault(cfPriceClassKey, awscloudfront.PriceClassPriceClassAll)
+	viper.SetDefault(cfWafArnKey, "")
+	viper.SetDefault(cfCustomSSLCertArnKey, "")
+	viper.SetDefault(cfSecurityPolicyKey, "")
+	viper.SetDefault(cfEnableLoggingKey, "false")
+	viper.SetDefault(cfS3BucketLogKey, "")
+	viper.SetDefault(cfEnableIPV6Key, "true")
+	viper.SetDefault(cfDescriptionTemplateKey, "")
+	viper.SetDefault(cfAliasCreationKey, "true")
+	viper.SetDefault(cfCustomTagsKey, "")
 	viper.AutomaticEnv()
 }
 
@@ -40,6 +65,30 @@ type OperatorCfg struct {
 	LogLevel string
 	// DevMode when set to "true" logs in unstructured text instead of JSON.
 	DevMode bool
+	// DefaultOriginDomain represents a valid domain to define in default origin.
+	DefaultOriginDomain string
+	// CloudFrontPriceClass determines how many edge locations CloudFront will use for your distribution.
+	// ref: https://docs.aws.amazon.com/sdk-for-go/api/service/cloudfront/
+	CloudFrontPriceClass string
+	// CloudFrontWAFARN the Web ACL ARN.
+	CloudFrontWAFARN string
+	// CloudFrontCustomSSLCertARN the ACM certificate ARN.
+	CloudFrontCustomSSLCertARN string
+	// CloudFrontSecurityPolicy the minimum SSL/TLS protocol that CloudFront can use to communicate with viewers.
+	// ref: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cloudfront-distribution-viewercertificate.html
+	CloudFrontSecurityPolicy string
+	// CloudFrontEnableLogging if should enable cloudfront logging.
+	CloudFrontEnableLogging bool
+	// CloudFrontS3BucketLog if logging enabled represents the S3 Bucket URL to persists, for example myawslogbucket.s3.amazonaws.com.
+	CloudFrontS3BucketLog string
+	// CloudFrontEnableIPV6 if should enable ipv6 for distribution responses.
+	CloudFrontEnableIPV6 bool
+	// CloudFrontDescriptionTemplate the description template for distribution.
+	CloudFrontDescriptionTemplate string
+	// CloudFrontAliasCreation if should create a DNS alias for distribution.
+	CloudFrontAliasCreation bool
+	// CloudFrontCustomTags all custom tags that will be persisted to distribution.
+	CloudFrontCustomTags map[string]string
 }
 
 // Parse environment variables into a config struct
@@ -49,8 +98,31 @@ func Parse() OperatorCfg {
 	if devMode {
 		logLvl = "debug"
 	}
+
 	return OperatorCfg{
-		LogLevel: logLvl,
-		DevMode:  devMode,
+		LogLevel:                      logLvl,
+		DevMode:                       devMode,
+		DefaultOriginDomain:           viper.GetString(cfDefaultOriginDomain),
+		CloudFrontPriceClass:          viper.GetString(cfPriceClassKey),
+		CloudFrontWAFARN:              viper.GetString(cfWafArnKey),
+		CloudFrontCustomSSLCertARN:    viper.GetString(cfCustomSSLCertArnKey),
+		CloudFrontSecurityPolicy:      viper.GetString(cfSecurityPolicyKey),
+		CloudFrontEnableLogging:       viper.GetBool(cfEnableLoggingKey),
+		CloudFrontS3BucketLog:         viper.GetString(cfS3BucketLogKey),
+		CloudFrontEnableIPV6:          viper.GetBool(cfEnableIPV6Key),
+		CloudFrontDescriptionTemplate: viper.GetString(cfDescriptionTemplateKey),
+		CloudFrontAliasCreation:       viper.GetBool(cfAliasCreationKey),
+		CloudFrontCustomTags:          extractTags(viper.GetString(cfCustomTagsKey)),
 	}
+}
+
+func extractTags(customTags string) map[string]string {
+	m := make(map[string]string)
+
+	tags := strings.Split(customTags, ",")
+	for _, pair := range tags {
+		tag := strings.Split(pair, "=")
+		m[tag[0]] = tag[1]
+	}
+	return m
 }
---
+++ internal/config/config_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2021 GPBR Participacoes LTDA.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package config_test
+
+import (
+	"testing"
+
+	"github.com/Gympass/cdn-origin-controller/internal/config"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestRunConfigTestSuite(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, &ConfigTestSuite{})
+}
+
+type ConfigTestSuite struct {
+	suite.Suite
+}
+
+func (s *ConfigTestSuite) TestConfigWithCustomTagsParsed() {
+	expected := map[string]string{
+		"foo":  "bar",
+		"area": "platform",
+	}
+
+	viper.Set("cf_custom_tags", "foo=bar,area=platform")
+
+	cfg := config.Parse()
+
+	s.Equal(expected, cfg.CloudFrontCustomTags)
+}
